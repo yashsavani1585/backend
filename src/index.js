7d30a2a6d1cc873c4eaf896b0a23f29c885fd741
yashsavani <yashsavani1585@gmail.com>
2024-08-09 18:02:31 +0530

    connect to mongoDb

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require("dotenv").config({path: "./.env"  });
+
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
+
+
+dotenv.config({ path: "./.env" });
+connectDb();
+
+// import express from "express";
+
+// const app = express();
+
+// (async ()=> {
+//     try {
+//      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//      app.on("error",(error)=>{
+//         console.error("MongoDB connection error:", error);
+//         throw error;
+//      })
+
+//      app.listen(process.env.PORT, () => {
+//         console.log(`Server is running on port ${process.env.PORT}`);
+//      })
+//     } catch (error) {
+//         console.log("error :",error);
+//         throw error;
+//     }
+// })()
