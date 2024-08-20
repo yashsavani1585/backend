7171acaac55ecf6098f1306a16f1e9f28620513a
yashsavani <yashsavani1585@gmail.com>
2024-08-20 15:34:08 +0530

    some error are solving

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
-import {registerUser} from "../controllers/user.controller.js";
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js";
 import {upload} from  "../middlewares/multer.middleware.js"
+import { verifyJWT } from '../middlewares/auth.middlewares.js';
 
 const router = Router()
 
@@ -16,6 +17,16 @@ router.route("/register").post(
         }
     ]),
     registerUser
+    )
+
+    router.route("/login").post(
+        loginUser
+    )
+
+    // secured routes goes here after adding auth middleware
+
+    router.route("/logout").post(
+        verifyJWT, logoutUser
     )
 
 
--- src/utils/ApiResponce.js
+++ src/utils/ApiResponce.js
@@ -1,10 +1,10 @@
-class ApiResponce {
-    constructor(statusCode, message, data = "success") {
-        this.statusCode = statusCode;
-        this.message = message;
-        this.data = data;
-        this.success = statusCode < 400;
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
     }
 }
 
-export { ApiResponce};
+export { ApiResponse }
