88ab09f5c9fe598747d527c085882df5838f3ed3
yashsavani <yashsavani1585@gmail.com>
2024-08-21 14:52:49 +0530

    add a new routes for a controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,18 @@
-import {Router} from 'express';
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from "../controllers/user.controller.js";
-import {upload} from  "../middlewares/multer.middleware.js"
+import { Router } from 'express';
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from '../middlewares/auth.middlewares.js';
 
 const router = Router()
@@ -10,26 +22,33 @@ router.route("/register").post(
         {
             name: "avatar",
             maxCount: 1
-        }, 
+        },
         {
             name: "coverImage",
             maxCount: 1
         }
     ]),
     registerUser
-    )
+)
 
-    router.route("/login").post(
-        loginUser
-    )
+router.route("/login").post(
+    loginUser
+)
 
-    // secured routes goes here after adding auth middleware
+// secured routes goes here after adding auth middleware
 
-    router.route("/logout").post(
-        verifyJWT, logoutUser
-    )
+router.route("/logout").post(
+    verifyJWT, logoutUser
+)
 
-    router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/C/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 export default router;
